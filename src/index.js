99cb99c3988bd62e747567eeb852a69b3b556d7b
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-09-12 12:33:56 -0500

    update

--- src/index.js
+++ src/index.js
@@ -9,34 +9,51 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 
 const server = app.listen(port, () => {
-  console.log('Escuchando en el puerto de middleware: ' + port);
+    console.log('Escuchando en el puerto de middleware: ' + port);
 });
 
-let socket = io(server);
 let existLeader = 0;
 let list_nodes = [];
-let nodes;
 
-socket.on('connection',async node => {
-    console.log('Nueva conexión:', node.handshake.headers.origin);
-    node.join('nodes');
-    nodes = socket.sockets.adapter.rooms['nodes'];
 
-    //agregar los nodos entrantes a la lista
+
+app.post('/newConn', function (req, res, next) {
+    var data = req.body;
+    console.log(data);
+    if(!isExist(list_nodes,data.url)){
         list_nodes.push({
-           url: node.handshake.headers.origin,
-           leader: (existLeader == 0)? 1: 0,
-           id_socket: node.id
+            url: data.url,
+            value: data.value,
+            leader: (existLeader == 0) ? 1 : 0
         });
-        //cuando se tiene el lider no se asigna otro lider
         existLeader = 1;
+    }
+    res.json({
+        list: JSON.stringify(list_nodes)
+    });
 
-        //se envia la lista a cada uno de los nodos
-    socket.to('nodes').emit('res:list',{
-        list:JSON.stringify(list_nodes)
+    //envio a todos menos a nuevo server que se conecto
+    list_nodes.forEach(element => {
+        if (element.url != data.url) {
+            axios.post(element.url+'/updateList', {
+                list: JSON.stringify(list_nodes)
+            }).then((response) => {
+                console.log(response.data);
+            }).cat
+        }
     });
 });
+
+function isExist(listNodes,urlNew){
+    var isExist = false;
+    listNodes.forEach(element => {
+        if(element.url == urlNew){
+            isExist = true;
+        }
+    });
+    return isExist;
+}
